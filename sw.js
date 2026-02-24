c6fb3b69399e790280959589a88272a0a1eaf19e
BernardoAraujo <bernardo.m.araujo@gmail.com>
2026-02-24 11:21:27 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* sw.js — SEM PLANO Meteo */
 
-const CACHE_VERSION = "semplano-meteo-v20260224_1";
+const CACHE_VERSION = "semplano-meteo-v20260224_2";
 
 const APP_SHELL = [
   "./",
@@ -105,6 +105,7 @@ self.addEventListener("fetch", (event) => {
     }
   })());
 });
+
 
 
 
